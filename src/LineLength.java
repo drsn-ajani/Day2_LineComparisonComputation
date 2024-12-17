4d877e907585dc8245614529061fc5b4855ceff0
drsn-ajani <daajani9103@gmail.com>
2024-12-17 18:16:34 +0530

    UC 2: Check whether lines are equals

--- src/LineLength.java
+++ src/LineLength.java
@@ -14,7 +14,21 @@ public class LineLength {
 
     public static void main(String[] args) {
         LineLength line = new LineLength(1, 2, 4, 6);
+        LineLength line2 = new LineLength(1, 2, 4, 6);
         double length = line.calculateLength();
-        System.out.println("The length of the line is: " + length);
+
+        // UC-1
+        System.out.println("The length of the line-1 is: " + length);
+        double length2 = line2.calculateLength();
+        System.out.println("The length of the line-2 is: " + length2);
+
+        // UC-2
+        if (length == length2) {
+            System.out.println("The length of the line-1 and line-2 is same");
+        } else {
+            System.out.println("The length of the line-1 and line-2 is different");
+        }
+
+
     }
 }
